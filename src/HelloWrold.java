496e8d26c1502b3d71ba794eecc031647db9f27c
hakuna <hakuna@218.38.137.27>
2018-02-08 15:55:34 +0900

    hello world code add.

--- src/HelloWrold.java
+++ src/HelloWrold.java
@@ -1,5 +1,9 @@
 public class HelloWrold {
 	public static void main(String[] grgs) {
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
 }
